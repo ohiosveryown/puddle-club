695b40cb672afcc2bfa110c62bb16284e71b9fe4
ohiosveryown <matt@ohiosveryown.co>
2026-03-03 10:38:20 -0500

    Initial Commit

---
+++ puddle-club/puddle_clubApp.swift
@@ -0,0 +1,17 @@
+//
+//  puddle_clubApp.swift
+//  puddle-club
+//
+//  Created by Matthew Pence on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct puddle_clubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
